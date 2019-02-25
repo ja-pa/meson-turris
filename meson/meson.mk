36c8455bbc982d666c6b9ae2e7ea903d09ccb2a5
Jan Pavlinec <jan.pavlinec@nic.cz>
2019-02-25 22:34:39 +0100

    meson: add meson pkg

---
+++ meson/meson.mk
@@ -0,0 +1,25 @@
+CROSS_CONF=$(STAGING_DIR)/cross_conf.txt
+PYTHON3_BIN=$(STAGING_DIR_HOST)/bin/python3
+MESON_BIN=$(STAGING_DIR_HOST)/meson/meson.py
+NINJA_BIN=$(STAGING_DIR_HOST)/usr/bin/ninja
+MESON_BUILD_DIR=$(PKG_BUILD_DIR)/builddir
+
+define Meson/Configure
+	echo Configure
+	echo $(PKG_CONFIG_PATH)
+	cd $(PKG_BUILD_DIR) && mkdir $(MESON_BUILD_DIR)
+	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(PYTHON3_BIN) $(MESON_BIN) builddir --cross-file $(CROSS_CONF)
+endef
+
+define Meson/Compile
+	echo Compile
+	cd $(MESON_BUILD_DIR) && $(NINJA_BIN)
+endef
+
+define Meson/Install
+	echo Compile
+	echo $(PKG_BUILD_DIR)
+endef
+
+
+
