ebfcb45635022341d87f5cccf994d93f32e5154c
Jan Pavlinec <jan.pavlinec@nic.cz>
2019-03-07 20:37:12 +0100

    meson.mk: cleanup

--- meson/meson.mk
+++ meson/meson.mk
@@ -3,11 +3,9 @@ __meson_mk_inc=1
 
 include $(TOPDIR)/package/ninja/ninja.mk
 
-CROSS_CONF=$(STAGING_DIR)/cross_conf.txt
-PYTHON3_BIN=$(STAGING_DIR_HOST)/bin/python3
-MESON_BIN=$(STAGING_DIR_HOST)/meson/meson.py
-#NINJA_BIN=$(STAGING_DIR_HOST)/usr/bin/ninja
-#MESON_BUILD_DIR=$(PKG_BUILD_DIR)/builddir
+CROSS_CONF_FILE=$(STAGING_DIR)/cross_conf.txt
+HOST_PYTHON3_BIN ?= $(STAGING_DIR_HOST)/bin/python3
+HOST_MESON_BIN=$(STAGING_DIR_HOST)/meson/meson.py
 MESON_BUILD_DIR=$(PKG_BUILD_DIR)
 
 PKG_BUILD_DEPENDS += meson/host
@@ -15,21 +13,11 @@ PKG_BUILD_DEPENDS += meson/host
 define Build/Meson/Configure
 	echo Configure
 	echo $(PKG_CONFIG_PATH)
-	#cd $(PKG_BUILD_DIR) && mkdir $(MESON_BUILD_DIR)
-	#cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(PYTHON3_BIN) $(MESON_BIN) builddir --cross-file $(CROSS_CONF)
-	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(PYTHON3_BIN) $(MESON_BIN) . --cross-file $(CROSS_CONF)
-	rm $(PKG_BUILD_DIR)/Makefile
+	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(HOST_PYTHON3_BIN) $(HOST_MESON_BIN) . --cross-file $(CROSS_CONF_FILE) $(MESON_ARGS)
 endef
 
 define Build/Meson/Compile
-	echo Compile
-	(cd $(MESON_BUILD_DIR) && $(NINJA_BIN))
+	$(call Build/Compile/Ninja)
 endef
-
-#define Meson/Install
-#	echo Compile
-#	echo $(PKG_BUILD_DIR)
-#endef
-
 
 endif
