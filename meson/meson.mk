5f97353433266db467589e764e6449d2585e5afb
Jan Pavlinec <jan.pavlinec@nic.cz>
2019-03-09 00:32:09 +0100

    meson: fix python3 path

--- meson/meson.mk
+++ meson/meson.mk
@@ -4,16 +4,16 @@ __meson_mk_inc=1
 include $(TOPDIR)/package/ninja/ninja.mk
 
 CROSS_CONF_FILE=$(STAGING_DIR)/cross_conf.txt
-HOST_PYTHON3_BIN ?= $(STAGING_DIR_HOST)/bin/python3
+HOST_PYTHON3_BIN ?= $(TOPDIR)/staging_dir/hostpkg/bin/python3
 HOST_MESON_BIN=$(STAGING_DIR_HOST)/meson/meson.py
-MESON_BUILD_DIR=$(PKG_BUILD_DIR)
-
-PKG_BUILD_DEPENDS += meson/host
+MESON_BUILD_DIR=$(PKG_BUILD_DIR)/builddir
+PKG_BUILD_DEPENDS += meson/host python3/host
 
 define Build/Meson/Configure
 	echo Configure
 	echo $(PKG_CONFIG_PATH)
-	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(HOST_PYTHON3_BIN) $(HOST_MESON_BIN) . --cross-file $(CROSS_CONF_FILE) $(MESON_ARGS)
+	cd $(PKG_BUILD_DIR) && mkdir $(MESON_BUILD_DIR)
+	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(HOST_PYTHON3_BIN) $(HOST_MESON_BIN) builddir --cross-file $(CROSS_CONF_FILE) $(MESON_ARGS)
 endef
 
 define Build/Meson/Compile
