42d82c2b253d2129c74228097d44e428445dcf07
Jan Pavlinec <jan.pavlinec@nic.cz>
2019-03-13 18:07:42 +0100

    meson: add install meson

--- meson/meson.mk
+++ meson/meson.mk
@@ -6,18 +6,20 @@ include $(TOPDIR)/package/ninja/ninja.mk
 CROSS_CONF_FILE=$(STAGING_DIR)/cross_conf.txt
 HOST_PYTHON3_BIN ?= $(TOPDIR)/staging_dir/hostpkg/bin/python3
 HOST_MESON_BIN=$(STAGING_DIR_HOST)/meson/meson.py
-MESON_BUILD_DIR=$(PKG_BUILD_DIR)/builddir
+MESON_BUILD_DIR ?= builddir
 PKG_BUILD_DEPENDS += meson/host python3/host
 
 define Build/Meson/Configure
-	echo Configure
-	echo $(PKG_CONFIG_PATH)
-	cd $(PKG_BUILD_DIR) && mkdir $(MESON_BUILD_DIR)
-	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(HOST_PYTHON3_BIN) $(HOST_MESON_BIN) builddir --cross-file $(CROSS_CONF_FILE) $(MESON_ARGS)
+	cd $(PKG_BUILD_DIR) && [ ! -d $(MESON_BUILD_DIR) ] && mkdir $(MESON_BUILD_DIR)
+	cd $(PKG_BUILD_DIR) && PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) $(HOST_PYTHON3_BIN) $(HOST_MESON_BIN) $(MESON_BUILD_DIR) --cross-file $(CROSS_CONF_FILE) $(MESON_ARGS)
 endef
 
 define Build/Meson/Compile
-	$(call Build/Compile/Ninja)
+	$(call Build/Ninja/Compile,$(MESON_BUILD_DIR))
+endef
+
+define Build/Meson/Install
+	$(call Build/Ninja/Install,$(MESON_BUILD_DIR))
 endef
 
 endif
